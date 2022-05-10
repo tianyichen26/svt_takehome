b19c8fd672c8391e318226967be15ec7874f1999
’tianyichen26‘ <chen.tianyi@northeastern.edu>
2022-05-10 13:25:51 -0400

    firstcommit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # svt_takehome
+
+run the code:
+python3 takehome-robot.py -l 'loadID' -x 'x-cord' -y 'y-cord'
+
+to install the library used in the code:
+sudo pip3 install requests
+sudo pip3 install json
+
+
+Further implementation:
+when there is 10000 robot and 1000 in the 10 unit range then some sort and search algorithm should be used for improve speed
+loadId is unused but it can contains some info that helps the search
---
+++ takehome-robot.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import math
+
+
+# function for get http response from given api
+def get_robot_info():
+    global response_info
+    response = requests.get("https://60c8ed887dafc90017ffbd56.mockapi.io/robots").text
+    response_info = json.loads(response)
+
+
+
+def find_best_robot(loadId,x,y):
+    global response_info
+    min_distance = 100000
+    robot_dict = {}
+    robot_id = 0
+    # loop through http response to get the robot within 10 unit into a dictionary
+    for r in response_info:
+        dis_square = abs(x-r['x'])**2 + abs(y-r['y'])**2
+        distance = math.sqrt(dis_square)
+        if distance < 10:
+            robot_dict[r['robotId']] = [r['batteryLevel'],distance]
+    max_bat = 0
+
+    # for robots in 10 unit, compare the battery life and choose the highest battery robot
+    for key in robot_dict:
+       if robot_dict[key][0]>max_bat:
+           max_bat = robot_dict[key][0]
+           robot_id = key
+           select_distance = robot_dict[key][1]
+    return robot_id,select_distance,max_bat
+
+
+def main():
+    get_robot_info()
+    r_id,distance,batterlevel = find_best_robot('test',50,50)
+    print(r_id)
+main()
